e9b212849987ff8396fc48a9046a982b03ed4e9d
Romi <rominexus91@gmail.com>
2021-07-12 01:33:49 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # RepoCommitTimeline
+
+## Notes
+
+If you want to compile and run this project, don't forget to create a file (called keys.properties) in the root of project containing [Github's API keys](https://github.com/settings/applications/new):
+
+    GithubClientId="YOUR_ID"
+    GithubClientSecret="YOUR_SECRET"
--- app/src/main/java/kost/romi/repocommittimeline/MainActivity.kt
+++ app/src/main/java/kost/romi/repocommittimeline/MainActivity.kt
@@ -2,10 +2,14 @@ package kost.romi.repocommittimeline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.build_config_test).text =
+            "${BuildConfig.GithubClientId}\n${BuildConfig.GithubClientSecret}"
     }
 }
