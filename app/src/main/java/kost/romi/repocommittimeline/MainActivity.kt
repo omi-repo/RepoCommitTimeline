7fb121079365c939783353c80d91e10bc9a2bb16
Romi <rominexus91@gmail.com>
2021-07-12 04:06:51 +0700

    Clear some warnings.

--- app/src/main/java/kost/romi/repocommittimeline/MainActivity.kt
+++ app/src/main/java/kost/romi/repocommittimeline/MainActivity.kt
@@ -2,7 +2,6 @@ package kost.romi.repocommittimeline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
