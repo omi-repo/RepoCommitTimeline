339b0338375c2050593dd41d4addd23c1970f16b
Romi <rominexus91@gmail.com>
2021-08-01 07:36:07 +0700

    Set theming.

--- app/src/main/java/kost/romi/repocommittimeline/MainActivity.kt
+++ app/src/main/java/kost/romi/repocommittimeline/MainActivity.kt
@@ -1,28 +1,48 @@
 package kost.romi.repocommittimeline
 
+import android.content.Context
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.bottomappbar.BottomAppBar
 import dagger.hilt.android.AndroidEntryPoint
 
 
 /**
- * TODO: use setTheme and recreate in activity to change and update theme.
+ *
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val sharedPrefs by lazy { getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        setTheme(R.)
+        setTheme(AppCompatDelegate.MODE_NIGHT_NO, THEME_LIGHT)
 
         val policy = ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
-//        findViewById<TextView>(R.id.build_config_test).text =
-//            "${BuildConfig.GithubClientId}\n${BuildConfig.GithubClientSecret}\n${BuildConfig.Token}"
+    }
+
+    private fun setTheme(themeMode: Int, prefsMode: Int) {
+        AppCompatDelegate.setDefaultNightMode(themeMode)
+        saveTheme(prefsMode)
+    }
+
+    private fun saveTheme(theme: Int) = sharedPrefs.edit().putInt(KEY_THEME, theme).apply()
+
+    companion object {
+        const val PREFS_NAME = "theme_prefs"
+        const val KEY_THEME = "prefs.theme"
+        const val THEME_UNDEFINED = -1
+        const val THEME_LIGHT = 0
+        const val THEME_DARK = 1
+        const val THEME_SYSTEM = 2
+        const val THEME_BATTERY = 3
     }
 }
